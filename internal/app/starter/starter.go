d7f7ca6e5e374a49207b7d1c4655d16c93590b31
Adrien STEFANSKI <adrien@knutech.fr>
2022-05-20 20:42:15 +0200

    chore(test): test

--- internal/app/starter/starter.go
+++ internal/app/starter/starter.go
@@ -6,10 +6,11 @@ import (
 	"wotracker-back/internal/app/api"
 )
 
-func RunApi() {
+func RunApi() api.Server {
 	log.Debug("loading config from configs folder")
 	loadedConfig := config.ReadConfigFile("configs")
 	log.Debugf("starting server on port %s", loadedConfig.PortServer)
 	server := api.Server{}
 	server.StartServer(loadedConfig)
+	return server
 }
